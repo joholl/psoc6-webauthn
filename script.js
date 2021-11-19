097e95f528882fc5571bac134a029dfdaac74acf
Johannes Holland <johannes.holland@infineon.com>
2021-11-19 01:16:02 +0100

    add a ton of printf debugging

--- script.js
+++ script.js
@@ -77,18 +77,25 @@ document.getElementById('open_door').addEventListener('click', async () => {
 	.then(device => device.gatt.connect())
 	.then(server => server.getPrimaryService('d4712330-b5d6-40ad-9035-70d2798bc1dc'))
 	.then(async (service) => {
+		console.log('Before get challenge.');
 		challenge = await service.getCharacteristic('d4712332-b5d6-40ad-9035-70d2798bc1dc');
 		value = await challenge.readValue();
 		console.log('Challenge:', value);
+		console.log('Before get cred.');
 		credential = await get_credential(challenge.buffer, false);
+		console.log('Before get 2.');
 		authdata = await service.getCharacteristic('d4712334-b5d6-40ad-9035-70d2798bc1dc');
 		unused = await authdata.writeValueWithResponse((new Uint8Array(credential.response.authenticatorData)).buffer);
+		console.log('Before get 3.');
 		clientdata = await service.getCharacteristic('d4712335-b5d6-40ad-9035-70d2798bc1dc');
 		unused = await clientdata.writeValueWithResponse((new Uint8Array(credential.response.clientDataJSON)).buffer);
+		console.log('Before get 4.');
 		response = await service.getCharacteristic('d4712333-b5d6-40ad-9035-70d2798bc1dc');
+		console.log('After get Char.');
 		var r = (new Uint8Array(credential.response.signature)).slice(4, 36);
 		var s = (new Uint8Array(credential.response.signature)).slice(-32);
 		var rs = new Uint8Array(Array.from(r).concat(Array.from(s)));
+		console.log('Before request.');
 		return response.writeValueWithResponse(rs.buffer);
 	})
 	.then(_ => {
